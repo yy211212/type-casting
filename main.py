bc7ab91b3337983c9bf6072beaaa59b20f6e005f
yy211212 <limyuyoung211212@gmail.com>
2024-10-31 16:12:00 +0800

    commit

---
+++ main.py
@@ -0,0 +1,19 @@
+name= "yu young"
+age=11
+is_student=True
+weight=38.5
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data Type of age is", type(age))
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of weight is", type(weight))
